89f0f58e10811f7625984a2f7672e142b1607ad4
Uonlra <120707623+uonlraSnaey@users.noreply.github.com>
2025-03-06 18:24:23 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,7 +9,8 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://www.uonlra.blog/' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '尝试联系我，我将很高兴能认识你', url:'ciencatsanye@gmail.com' },
+    { title: '这里是nition相关文档', url: 'https://docs.tangly1024.com' }
   ],
 
   // 英雄区左右侧组件颠倒位置
